b2dacdda0e222cfc29ecdb708123ca8f00f0d19e
Jan Bölsche <regular.gonzales@googlemail.com>
2017-01-31 19:57:52 +0100

    Add support for systemd-notify. Add command line execution (from a well-defined pre-configured preset) via websockets. Add html music player client as proof of concept

--- client.js
+++ client.js
@@ -1,3 +1,4 @@
+//jshint esversion: 6, -W083
 var shoe = require('shoe');
 var hterm = require('hterm-umdjs').hterm; 
 var lib = require('hterm-umdjs').lib; 
@@ -42,12 +43,53 @@ t.onTerminalReady = function() {
     t.prefs_.set('cursor-blink', true);
 
     t.prefs_.set('enable-bold', true);
-    t.term_.prefs_.set('enable-bold-as-bright', false);
+    t.prefs_.set('enable-bold-as-bright', false);
 
     t.prefs_.set('environment', {
       "TERM": "xterm-256color"
     });
     solarized.dark(t);
+
+    // mpc test
+    var split = require('split');
+    let mpc = shoe('/mpc');
+    setInterval( ()=> {
+        mpc.write('status\n');
+        return true;
+    }, 5000);
+    for(let btn of 'prev play pause next'.split(' ')) {
+        document.querySelector(`#mpc .${btn}`).addEventListener('click', ()=>{
+            //t.io.println(`${btn}!`);
+            mpc.write(`${btn}\nstatus\n`);
+        });
+    }
+    mpc.pipe(split()).on('data', function (msg) {
+        let meta = msg.match(/artist=([^\t]*)\ttitle=([^\t]*)\tduration=(\d*)\s*/);
+        let progress = msg.match(/\[(\w+)\]\s+\#(\d+)\/(\d+)\s+(\d+)\:(\d+)\/(\d+)\:(\d+)\s*.*/);
+        let status = msg.match(/volume: (\d+)%\s+repeat: (on|off)\s+random: (on|off)\s+single: (on|off)\s+consume: (on|off)/);
+        if (meta) {
+            let [_,artist,title,duration] = meta;
+            //t.io.println(`title: ${title}`);
+            let el = document.querySelector('#mpc .title');
+            el.innerHTML = title;
+        } else if (progress) {
+            let [_, transport, listPos, listLen, curMin, curSec, durMin, durSec] = progress;
+            if (transport === 'playing') {
+                document.querySelector('#mpc .play').style.display = 'none';
+                document.querySelector('#mpc .pause').style.display = 'initial';
+            } if (transport === 'paused') {
+                document.querySelector('#mpc .play').style.display = 'initial';
+                document.querySelector('#mpc .pause').style.display = 'none';
+            }
+            //t.io.println(`transport: ${transport} pos: ${curMin}:${curSec}`);
+        } else if (status) {
+            let [_, volume, repeat, random, single, consume] = status;
+            //t.io.println(`volume: ${volume}`);
+        } else {
+            //t.io.println(msg);
+        }
+    });
+    // --
 };
 t.decorate(document.querySelector('#terminal'));
 t.installKeyboard();
--- server.js
+++ server.js
@@ -1,31 +1,93 @@
-// jshint: esversion 6
-var os = require('os');
-var pty = require('node-pty');
-var shoe = require('shoe');
-var http = require('http');
- 
-var shell = '/usr/bin/login';
- 
-var ecstatic = require('ecstatic')(__dirname, {
+// jshint esversion: 6, -W083
+const sd = require('systemd-daemon');
+const fs = require('fs');
+const spawn = require('child_process').spawn;
+const R = require('ramda');
+const os = require('os');
+const pty = require('node-pty');
+const shoe = require('shoe');
+const http = require('http');
+const ecstatic = require('ecstatic')(__dirname, {
     cache: "no-cache"
 });
+const split = require('split');
+
+const parseConfig = require('./parse-config');
+
+const shell = '/usr/bin/login';
+const logFilePath = '/var/log/node-terminal-server.log';
  
-var server = http.createServer(ecstatic);
-server.listen(9999);
- 
-var sock = shoe(function (stream) {
-    console.log("something is happening");
-    var ptyProcess = pty.spawn(shell, [], {
-      name: 'xterm-color',
-      cols: 80,
-      rows: 25,
-      cwd: process.env.HOME,
-      env: process.env
-    });
-     
-    ptyProcess.pipe(stream).pipe(ptyProcess, {end: false});
-    //stream.pipe(process.stdout, {end: false});
-    //process.stdin.pipe(stream);
+const console = require('tracer').console({
+    transport: (data) => {
+        fs.appendFile(logFilePath, data.output + '\n', (err) => {
+            if (err) throw err;
+        });
+    }
 });
 
-sock.install(server, '/pty');
+console.log('Parsing config');
+parseConfig( (err, config) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+
+    let server = http.createServer(ecstatic);
+    server.listen(9999, "127.0.0.1", ()=>{
+        sd.notify('READY=1');
+        console.log('ready, listening on 9999 (localhost only)');
+    });
+     
+    // Create PTY socket
+    let sock = shoe(function (stream) {
+        console.log("New PTY connection");
+        let ptyProcess = pty.spawn(shell, [], {
+          name: 'xterm-256color',
+          cols: 80,
+          rows: 25,
+          cwd: process.env.HOME,
+          env: process.env
+        });
+        ptyProcess.pipe(stream).pipe(ptyProcess, {end: false});
+    });
+    sock.install(server, '/pty');
+
+    // Create sockets for additional endpoints 
+    const createSocket = (mappings)=> {
+        let url = `/${mappings[0].endpoint}`;
+        console.log(`Installing socket server at ${url}`);
+        let sock = shoe(function (stream) {
+            console.log(`New connection to ${url}`);
+            stream.pipe(split()).on('data', (line)=>{
+                console.log('received',line);    
+                for(let {re, cli} of mappings) {
+                    let m = line.match(re);
+                    if (m) {
+                        console.log(`Matching ${re}`);
+                        console.log(m);
+                        console.log(`Running ${cli}`);
+                        let command = spawn(cli, {
+                            cwd: process.env.HOME,
+                            env: process.env,
+                            uid: 1000,  // TODO
+                            gid: 1000,
+                            shell: true
+                        });
+                        command.stdout.pipe(split()).on('data', (line)=>{
+                            console.log('stdout', line);
+                            stream.write(line + '\n');
+                        });
+                        command.on('close', (code)=>{
+                            console.log(`${cli} ended with code ${code}`);
+                        });
+                    }
+                }
+            });
+        });
+        sock.install(server, url);
+    };
+
+    let createSockets = R.forEach(createSocket);
+
+    createSockets(R.values(config));
+});
