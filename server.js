6ff220decd46fa97333b274dd8358eadc5097e4d
Jan Bölsche <regular.gonzales@googlemail.com>
2017-02-04 17:48:51 +0100

    Extract remote-spwan into it\'s own module, remove mpc example

--- server.js
+++ server.js
@@ -11,8 +11,7 @@ const ecstatic = require('ecstatic')(__dirname, {
     cache: "no-cache"
 });
 const split = require('split');
-
-const parseConfig = require('./parse-config');
+const remoteSpawn = require('remote-spawn');
 
 const shell = '/usr/bin/login';
 const logFilePath = '/var/log/node-terminal-server.log';
@@ -25,69 +24,29 @@ const console = require('tracer').console({
     }
 });
 
-console.log('Parsing config');
-parseConfig( (err, config) => {
+
+let server = http.createServer(ecstatic);
+server.listen(9999, "127.0.0.1", ()=>{
+    sd.notify('READY=1');
+    console.log('ready, listening on 9999 (localhost only)');
+});
+ 
+// Create PTY socket
+let sock = shoe(function (stream) {
+    console.log("New PTY connection");
+    let ptyProcess = pty.spawn(shell, [], {
+      name: 'xterm-256color',
+      cols: 80,
+      rows: 25,
+      cwd: process.env.HOME,
+      env: process.env
+    });
+    ptyProcess.pipe(stream).pipe(ptyProcess, {end: false});
+});
+sock.install(server, '/pty');
+
+remoteSpawn(server, (err)=> {
     if (err) {
-        console.error(err);
-        process.exit(1);
+        console.error(`Failed to initialze remote-spawn: ${err}`);
     }
-
-    let server = http.createServer(ecstatic);
-    server.listen(9999, "127.0.0.1", ()=>{
-        sd.notify('READY=1');
-        console.log('ready, listening on 9999 (localhost only)');
-    });
-     
-    // Create PTY socket
-    let sock = shoe(function (stream) {
-        console.log("New PTY connection");
-        let ptyProcess = pty.spawn(shell, [], {
-          name: 'xterm-256color',
-          cols: 80,
-          rows: 25,
-          cwd: process.env.HOME,
-          env: process.env
-        });
-        ptyProcess.pipe(stream).pipe(ptyProcess, {end: false});
-    });
-    sock.install(server, '/pty');
-
-    // Create sockets for additional endpoints 
-    const createSocket = (mappings)=> {
-        let url = `/${mappings[0].endpoint}`;
-        console.log(`Installing socket server at ${url}`);
-        let sock = shoe(function (stream) {
-            console.log(`New connection to ${url}`);
-            stream.pipe(split()).on('data', (line)=>{
-                console.log('received',line);    
-                for(let {re, cli} of mappings) {
-                    let m = line.match(re);
-                    if (m) {
-                        console.log(`Matching ${re}`);
-                        console.log(m);
-                        console.log(`Running ${cli}`);
-                        let command = spawn(cli, {
-                            cwd: process.env.HOME,
-                            env: process.env,
-                            uid: 1000,  // TODO
-                            gid: 1000,
-                            shell: true
-                        });
-                        command.stdout.pipe(split()).on('data', (line)=>{
-                            console.log('stdout', line);
-                            stream.write(line + '\n');
-                        });
-                        command.on('close', (code)=>{
-                            console.log(`${cli} ended with code ${code}`);
-                        });
-                    }
-                }
-            });
-        });
-        sock.install(server, url);
-    };
-
-    let createSockets = R.forEach(createSocket);
-
-    createSockets(R.values(config));
 });
